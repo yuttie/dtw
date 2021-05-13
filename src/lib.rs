d1ffef46d0c5cdf0a4148e01c7ccca819e2f2569
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-05-13 17:24:31 +0900

    dtw: Correctly handle empty sequences

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::vec::Vec;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use pyo3::exceptions::PyValueError;
 use ndarray::Ix2;
 use numpy::{PyArray, ToPyArray};
 
@@ -15,10 +16,13 @@ fn dtw(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 #[pyfunction]
-fn dp<'py>(py: Python<'py>, s: Vec<usize>, t: Vec<usize>, d: &'py PyArray<u32, Ix2>) -> &'py PyArray<u32, Ix2> {
+fn dp<'py>(py: Python<'py>, s: Vec<usize>, t: Vec<usize>, d: &'py PyArray<u32, Ix2>) -> PyResult<&'py PyArray<u32, Ix2>> {
     let d = d.readonly();
     let d = d.as_array();
-    py.allow_threads(|| rs::dp(&s, &t, &d)).to_pyarray(py)
+    match py.allow_threads(|| rs::dp(&s, &t, &d)) {
+        Some(table) => Ok(table.to_pyarray(py)),
+        None => Err(PyValueError::new_err("Empty sequence is not allowed.")),
+    }
 }
 
 #[pyfunction]
@@ -29,10 +33,10 @@ fn optimal_path<'py>(py: Python<'py>, table: &'py PyArray<u32, Ix2>) -> Vec<(usi
 }
 
 #[pyfunction]
-fn dist<'py>(py: Python<'py>, s: Vec<usize>, t: Vec<usize>, d: &'py PyArray<u32, Ix2>) -> u32 {
+fn dist<'py>(py: Python<'py>, s: Vec<usize>, t: Vec<usize>, d: &'py PyArray<u32, Ix2>) -> PyResult<u32> {
     let d = d.readonly();
     let d = d.as_array();
-    py.allow_threads(|| rs::dist(&s, &t, &d))
+    py.allow_threads(|| rs::dist(&s, &t, &d)).ok_or(PyValueError::new_err("Empty sequence is not allowed."))
 }
 
 #[pyfunction]
@@ -49,9 +53,14 @@ mod rs {
     use ndarray::parallel::prelude::par_azip;
 
     /// Compute a DP table.
-    pub fn dp<'a, S: Sync + Data + RawData<Elem = u32>>(s: &[usize], t: &[usize], d: &ArrayBase<S, Ix2>) -> Array2<u32> {
+    pub fn dp<'a, S: Sync + Data + RawData<Elem = u32>>(s: &[usize], t: &[usize], d: &ArrayBase<S, Ix2>) -> Option<Array2<u32>> {
         let n = s.len();
         let m = t.len();
+
+        if n == 0 || m == 0 {
+            return None;
+        }
+
         let mut table = Array2::<u32>::zeros((n + 1, m + 1));
 
         table[[0, 0]] = 0;
@@ -82,7 +91,7 @@ mod rs {
             }
         }
 
-        table
+        Some(table)
     }
 
     /// Compute the optimal path from a given DP table.
@@ -124,11 +133,10 @@ mod rs {
     }
 
     /// Compute the distance between two given sequences.
-    pub fn dist<'a, S: Sync + Data + RawData<Elem = u32>>(s: &[usize], t: &[usize], d: &ArrayBase<S, Ix2>) -> u32 {
+    pub fn dist<'a, S: Sync + Data + RawData<Elem = u32>>(s: &[usize], t: &[usize], d: &ArrayBase<S, Ix2>) -> Option<u32> {
         let n = s.len();
         let m = t.len();
-        let table = dp(s, t, &d);
-        table[[n, m]]
+        dp(s, t, &d).map(|table| table[[n, m]])
     }
 
     /// Compute pairwise distances between sequences.
@@ -137,7 +145,7 @@ mod rs {
         let mut dmat = Array2::<u32>::zeros((n, n));
         let indices = ndarray::indices_of(&dmat);
         par_azip!((dmat_ij in &mut dmat, (i, j) in indices) {
-            *dmat_ij = dist(xs[i].as_ref(), xs[j].as_ref(), &d);
+            *dmat_ij = dist(xs[i].as_ref(), xs[j].as_ref(), &d).unwrap();
         });
         dmat
     }
@@ -160,14 +168,33 @@ mod tests {
 
         let s = vec![0, 1, 2, 3, 4];
         let t = vec![0, 1, 2, 3, 4];
-        assert_eq!(rs::dp(&s, &t, &d), array![
+        assert_eq!(rs::dp(&s, &t, &d), Some(array![
             [0,  0, 0, 0, 0,  0],
             [0,  0, 1, 3, 6, 10],
             [0,  1, 0, 1, 3,  6],
             [0,  3, 1, 0, 1,  3],
             [0,  6, 3, 1, 0,  1],
             [0, 10, 6, 3, 1,  0],
-        ]);
+        ]));
+    }
+
+    #[test]
+    fn test_dp_with_empty_sequence() {
+        let d: Array2<u32> = array![
+            [0, 1, 2, 3, 4],
+            [1, 0, 1, 2, 3],
+            [2, 1, 0, 1, 2],
+            [3, 2, 1, 0, 1],
+            [4, 3, 2, 1, 0],
+        ];
+
+        let s = vec![];
+        let t = vec![0, 1, 2, 3, 4];
+        assert_eq!(rs::dp(&s, &t, &d), None);
+
+        let s = vec![0, 1, 2, 3, 4];
+        let t = vec![];
+        assert_eq!(rs::dp(&s, &t, &d), None);
     }
 
     #[test]
@@ -183,6 +210,8 @@ mod tests {
         let s = vec![0, 1, 2, 3, 4];
         let t = vec![0, 1, 2, 3, 4];
         let table = rs::dp(&s, &t, &d);
+        assert_eq!(table.is_some(), true);
+        let table = table.unwrap();
         assert_eq!(rs::optimal_path(&table), vec![
             (0, 0),
             (1, 1),
@@ -204,6 +233,25 @@ mod tests {
 
         let s = vec![0, 1, 2, 3, 4];
         let t = vec![0, 1, 2, 3, 4];
-        assert_eq!(rs::dist(&s, &t, &d), 0);
+        assert_eq!(rs::dist(&s, &t, &d), Some(0));
+    }
+
+    #[test]
+    fn test_dist_with_empty_sequence() {
+        let d = array![
+            [0, 1, 2, 3, 4],
+            [1, 0, 1, 2, 3],
+            [2, 1, 0, 1, 2],
+            [3, 2, 1, 0, 1],
+            [4, 3, 2, 1, 0],
+        ];
+
+        let s = vec![];
+        let t = vec![0, 1, 2, 3, 4];
+        assert_eq!(rs::dist(&s, &t, &d), None);
+
+        let s = vec![0, 1, 2, 3, 4];
+        let t = vec![];
+        assert_eq!(rs::dist(&s, &t, &d), None);
     }
 }
